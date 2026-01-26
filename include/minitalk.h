3d5ff81c09c940cccaf35fa626d30b4b3202a583
roandrie <roandrie@student.42lehavre.fr>
2026-01-26 08:27:00 +0100

    norme correction

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,12 +6,12 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 11:24:26 by roandrie          #+#    #+#             */
-/*   Updated: 2026/01/21 13:06:43 by roandrie         ###   ########.fr       */
+/*   Updated: 2026/01/26 08:26:40 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
-#	define MINITALK_H
+# define MINITALK_H
 
 # include "../libft/include/libft.h"
 # include <unistd.h>
